9e8186d1fc0ceb6a927646d081ace0de681e5222
Bahriddin <bahridnurullav@gmail.com>
2025-09-12 13:40:46 +0500

    single user

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -2,6 +2,7 @@ import httpRequest from "../utils/httpRequest";
 
 const usersService = {
   getList: () => httpRequest.get(`v2/items/users`, {}),
+  deleteUser: (id) => httpRequest.delete(`v2/items/users/${id}`, {}),
 };
 
 export default usersService;

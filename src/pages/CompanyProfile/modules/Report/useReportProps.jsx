52534bb553b2803f84aa15bb8b3b3c1ce2203be9
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2025-09-15 08:59:42 +0500

    feat(api): connect frontend with backend integration endpoint

--- src/pages/CompanyProfile/modules/Report/useReportProps.jsx
+++ src/pages/CompanyProfile/modules/Report/useReportProps.jsx
@@ -1,36 +1,26 @@
 import { useForm } from "react-hook-form";
-import { useGetUsdotInfo } from "../../services/report.service";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useGetCompanyInfo } from "../../services/report.service";
+import { format } from "date-fns";
 
 export const useReportProps = () => {
-  const { company_id, guid, client_type_id, role_id } = useSelector(state => state.auth.user_data);
+  const { company_id, guid } = useSelector(state => state.auth.user_data);
 
-  console.log({ company_id });
+  const { control, reset } = useForm();
 
-  const { control, watch, setValue, handleSubmit } = useForm();
-
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-
-  const usdotMutation = useGetUsdotInfo({ 
-    onSuccess: (data) => {
-      console.log(data);
-    }, 
-  });
+  const { data } = useGetCompanyInfo({}, company_id || guid);
 
   useEffect(() => {
-    usdotMutation.mutate({
-      data: {
-        offset: 0,
-        limit: 20,
-        companies_id: [
-          company_id || guid,
-        ],
-      },
-    });
-  }, []);
+    if (data?.response) {
+      const { out_of_service_date, mcs_150_form_date } = data.response;
+      reset({
+        ...data?.response,
+        out_of_service_date: format(new Date(out_of_service_date), "dd-MM-yyyy"),
+        mcs_150_form_date: format(new Date(mcs_150_form_date), "dd-MM-yyyy"),
+      });
+    }
+  }, [data]);
 
-  return { control, watch, setValue, handleSubmit, onSubmit };
+  return { control };
 };

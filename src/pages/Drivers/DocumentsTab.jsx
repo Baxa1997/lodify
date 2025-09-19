5bc8db457b2bd1eec0e8e084358e53fb951022f3
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2025-09-19 07:43:54 +0500

    add: drivers documents ui

--- src/pages/Drivers/DocumentsTab.jsx
+++ src/pages/Drivers/DocumentsTab.jsx
@@ -131,6 +131,98 @@ const DocumentsTab = () => {
         <FolderCard />
         <FolderCard disabled />
       </Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        mt="44px"
+      >
+        <MainHeading size="18px">Background Screenings</MainHeading>
+        <Link
+          href="#"
+          colorScheme="tertiary.600"
+          fontWeight="600"
+          _hover={
+            {
+              color: "tertiary.600",
+            }
+          }
+        >
+          <Box
+            display="flex"
+            alignItems="center"
+            gap="6px"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="none"><path
+                stroke="#535862"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.667"
+                d="M6.667 14.167 10 17.5m0 0 3.333-3.333M10 17.5V10m6.666 3.952a4.583 4.583 0 0 0-2.917-8.12.516.516 0 0 1-.444-.25 6.25 6.25 0 1 0-9.816 7.58"/></svg>
+            <span>Download all files</span>
+          </Box>
+        </Link>
+      </Box>
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(3, 1fr)"
+        gap="24px"
+        mt="36px"
+      >
+        <FolderCard />
+        <FolderCard />
+        <FolderCard disabled />
+      </Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        mt="44px"
+      >
+        <MainHeading size="18px">Other</MainHeading>
+        <Link
+          href="#"
+          colorScheme="tertiary.600"
+          fontWeight="600"
+          _hover={
+            {
+              color: "tertiary.600",
+            }
+          }
+        >
+          <Box
+            display="flex"
+            alignItems="center"
+            gap="6px"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="none"><path
+                stroke="#535862"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.667"
+                d="M6.667 14.167 10 17.5m0 0 3.333-3.333M10 17.5V10m6.666 3.952a4.583 4.583 0 0 0-2.917-8.12.516.516 0 0 1-.444-.25 6.25 6.25 0 1 0-9.816 7.58"/></svg>
+            <span>Download all files</span>
+          </Box>
+        </Link>
+      </Box>
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(3, 1fr)"
+        gap="24px"
+        mt="36px"
+      >
+        <FolderCard disabled />
+        <FolderCard disabled />
+        <FolderCard disabled />
+      </Box>
     </Box>
   </Box>;
 };
